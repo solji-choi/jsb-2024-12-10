b58312a033c1f8c0a810b6ec552c818ee3cffeac
solji <solji.choi89@gmail.com>
2024-12-10 10:48:28 +0900

    ResponsBody

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,19 +2,24 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
     @GetMapping("/")
-    public void home() {
+    @ResponseBody
+    public int home() {
         System.out.println("Home");
-        this.contact();
+
+        return 22;
     }
 
     @GetMapping("/about")
-    public void about() {
+    @ResponseBody
+    public String about() {
         System.out.println("About");
-        this.contact();
+
+        return "안녕하세요";
     }
 
     public void contact() {
