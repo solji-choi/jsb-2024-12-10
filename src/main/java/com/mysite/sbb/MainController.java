0b9ac0768522310fb0f780cba95dd0e50d098fe7
solji <solji.choi89@gmail.com>
2024-12-10 10:45:27 +0900

    no action metho name is must be readable

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -8,10 +8,16 @@ public class MainController {
     @GetMapping("/")
     public void home() {
         System.out.println("Home");
+        this.contact();
     }
 
     @GetMapping("/about")
     public void about() {
         System.out.println("About");
+        this.contact();
+    }
+
+    public void contact() {
+        System.out.println("Contact");
     }
 }
