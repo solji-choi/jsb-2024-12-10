4002f777ca6a7595b4fe84700750e458841f73ac
solji <solji.choi89@gmail.com>
2024-12-16 01:37:45 +0900

    question controller add questionCreate

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -3,9 +3,7 @@ package com.mysite.sbb.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,5 +32,15 @@ public class QuestionController {
     @GetMapping("/create")
     public String questionCreate() {
         return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(
+            @RequestParam(value = "subject") String subject,
+            @RequestParam(value = "content") String content
+    ) {
+        // TODO 질문을 저장한다.
+
+        return "redirect:/question/list";
     }
 }
