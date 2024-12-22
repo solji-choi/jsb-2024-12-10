1f308fb4d4afe6f7c2d48b0762904e30524d2a7d
solji <solji.choi89@gmail.com>
2024-12-22 23:09:17 +0900

    add list paging

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -2,6 +2,9 @@ package com.mysite.sbb.question;
 
 import com.mysite.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -31,5 +34,11 @@ public class QuestionService {
         q.setCreateDate(LocalDateTime.now());
 
         this.questionRepository.save(q);
+    }
+
+    public Page<Question> getList(int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+
+        return this.questionRepository.findAll(pageable);
     }
 }
