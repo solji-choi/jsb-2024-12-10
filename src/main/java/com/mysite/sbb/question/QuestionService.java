ae37cf425f18d2b1769dea6731b2684b5cfee195
solji <solji.choi89@gmail.com>
2024-12-25 23:46:41 +0900

    add question modify

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -48,4 +48,12 @@ public class QuestionService {
 
         return this.questionRepository.findAll(pageable);
     }
+
+    public void modify(Question question, String subject, String content) {
+        question.setSubject(subject);
+        question.setContent(content);
+        question.setModifyDate(LocalDateTime.now());
+
+        this.questionRepository.save(question);
+    }
 }
